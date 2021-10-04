b1a00fea8d8e635850aca693572466bd4cb66772
AmnaSZafar <amna.s.zafar@gmail.com>
2021-10-04 15:07:39 +0500

    config again

--- config/database.php
+++ config/database.php
@@ -64,7 +64,7 @@ return [
         ],
 
         'pgsql' => [
-            'pgsql' => [
+           
                 'driver' => 'pgsql',
                     'read' => [
                         'host' => [
